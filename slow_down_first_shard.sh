66c44d84d077bc0184ca4dcbe7ae197c1a8ee0fd
Artem Klyukvin <a.klyukvin@joom.com>
2022-10-19 12:57:32 +0400

    cleanup

--- slow_down_first_shard.sh
+++
@@ -1,3 +0,0 @@
-docker exec -it shard-01-node-a tc qdisc add dev eth0 root netem delay 1000ms
-#docker exec -it shard-01-node-b tc qdisc add dev eth0 root netem delay 500ms
-#docker exec -it shard-01-node-c tc qdisc add dev eth0 root netem delay 500ms
